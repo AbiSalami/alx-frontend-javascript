1c15dc69ad4917049e17c88de1d15730290f3544
AbiSalami <bimwealth215@gmail.com>
2024-07-27 00:59:23 +0000

    task 100

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,21 @@
+// 100-await.js
+import { uploadPhoto, createUser } from './utils';
+
+const asyncUploadUser = async () => {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+};
+
+export default asyncUploadUser;
+
